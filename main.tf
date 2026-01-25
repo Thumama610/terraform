65ede1c54f60a3a9f492afa5410adafc1b9fcd17
Thumama610 <thumamaalrawwad610@gmail.com>
2026-01-25 23:04:50 +0300

    adding a new instance

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "ec2" {
     Name = "EC2-from-GitHub-Actions"
   }
 }
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0b6c6ebed2801a5cb" # ubuntu 24.04 (us-east-1)
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "another one"
+  }
+}
