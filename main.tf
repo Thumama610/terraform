cc0f7ee7beb393ca8f4650974314e60f2718e06d
Thumama610 <thumamaalrawwad610@gmail.com>
2026-01-25 22:57:37 +0300

    upload all files for the first time

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami           = "ami-0b6c6ebed2801a5cb" # ubuntu 24.04 (us-east-1)
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "EC2-from-GitHub-Actions"
+  }
+}
