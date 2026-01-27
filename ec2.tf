0632cfaf4d9633d9b08e87462997a67637ff998a
Thumama610 <thumamaalrawwad610@gmail.com>
2026-01-27 18:28:31 +0300

    added the private key

--- ec2.tf
+++ ec2.tf
@@ -11,7 +11,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.xlarge"
-  key_name      = "id_rsa"
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
